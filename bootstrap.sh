d25291536f3c45004c0daf00eee2d69530d5f70c
Patrick Mizer <patrick@sparefoot.com>
2014-04-10 08:45:20 -0500

    Added hhvm and LAMP

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+#install PHP / Apache
+sudo apt-get install -y php5 libapache2-mod-php5
+
+# Install hhvm
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu saucy main | sudo tee /etc/apt/sources.list.d/hhvm.list
 sudo apt-get update
-sudo apt-get install hhvm
+sudo apt-get install -y hhvm
